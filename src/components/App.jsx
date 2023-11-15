b70626417744412a1e92bbb8f0bb1eaa635ec2c0
Valentyna1402 <vriabchun@gmail.com>
2023-11-15 14:59:28 +0100

    format

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
@@ -6,62 +6,59 @@ import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
 export const App = () => {
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-const updateGoodFeedback = () => {
-  setGood(prevGood => prevGood + 1);
+  const updateGoodFeedback = () => {
+    setGood(prevGood => prevGood + 1);
   };
 
-const updateNeutralFeedback = () => {
-  setNeutral(prevNeutral => prevNeutral + 1);
-    };
+  const updateNeutralFeedback = () => {
+    setNeutral(prevNeutral => prevNeutral + 1);
+  };
 
-const updateBadFeedback = () => {
-  setBad(prevBad => prevBad + 1);
-        };
+  const updateBadFeedback = () => {
+    setBad(prevBad => prevBad + 1);
+  };
 
-const countTotalFeedback = () => {
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-const countPositiveFeedbackPercentage = () => {
+  const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
     const percentage = (good / total) * 100;
 
     return total > 0 ? Math.round(percentage) : 0;
   };
 
-    const total = countTotalFeedback();
-    const positivePercentage = countPositiveFeedbackPercentage();
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-    return (
-      <div className="general-container">
-        <Section title="Please, leave your feedback">
-          <FeedbackOptions
-            onLeaveGoodFeedback={updateGoodFeedback}
-            onLeaveNeutralFeedback={updateNeutralFeedback}
-            onLeaveBadFeedback={updateBadFeedback}
+  return (
+    <div className="general-container">
+      <Section title="Please, leave your feedback">
+        <FeedbackOptions
+          onLeaveGoodFeedback={updateGoodFeedback}
+          onLeaveNeutralFeedback={updateNeutralFeedback}
+          onLeaveBadFeedback={updateBadFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </div>
-    );
-  }
-
-
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </div>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,20 +1,15 @@
 import { Wrapper, Button } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({onLeaveGoodFeedback, onLeaveNeutralFeedback, onLeaveBadFeedback }) => {
+export const FeedbackOptions = ({
+  onLeaveGoodFeedback,
+  onLeaveNeutralFeedback,
+  onLeaveBadFeedback,
+}) => {
   return (
     <Wrapper>
-          <Button
-            onClick={onLeaveGoodFeedback}
-          >Good
-          </Button>
-          <Button
-            onClick={onLeaveNeutralFeedback}
-          >Neutral
-          </Button>
-          <Button
-            onClick={onLeaveBadFeedback}
-          >Bad
-          </Button>
+      <Button onClick={onLeaveGoodFeedback}>Good</Button>
+      <Button onClick={onLeaveNeutralFeedback}>Neutral</Button>
+      <Button onClick={onLeaveBadFeedback}>Bad</Button>
     </Wrapper>
   );
 };
